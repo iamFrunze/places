d61aca1754b56599bdd2a93979f93730e49e8043
Антон Коротков <byfrunze@gmail.com>
2022-09-08 16:59:02 +0300

    adaptive date picker

--- lib/ui/screen/sight_details/widgets/body/bottom_buttons.dart
+++ lib/ui/screen/sight_details/widgets/body/bottom_buttons.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:places/res/app_assets.dart';
 import 'package:places/res/app_strings.dart';
@@ -18,22 +21,7 @@ class BottomButtonsWidget extends StatelessWidget {
       children: [
         PlanningTextButton(
           onPressed: () async {
-            final date = await showDatePicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime.now(),
-              lastDate: DateTime.now().add(const Duration(days: 365)),
-              builder: (context, child) => Theme(
-                data: Theme.of(context).copyWith(
-                  textButtonTheme: TextButtonThemeData(
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all(Colors.green),
-                    ),
-                  ),
-                ),
-                child: child!,
-              ),
-            );
+            final date = await showAdaptiveDatePicker(context);
             if (date != null) {
               await Future<void>.microtask(
                 () => context
@@ -65,5 +53,55 @@ class BottomButtonsWidget extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Future<DateTime?> showAdaptiveDatePicker(BuildContext context) async {
+    DateTime? selectDate;
+    if (Platform.isAndroid) {
+      selectDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime.now().add(const Duration(days: 365)),
+        builder: (context, child) => Theme(
+          data: Theme.of(context).copyWith(
+            textButtonTheme: TextButtonThemeData(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.green),
+              ),
+            ),
+          ),
+          child: child!,
+        ),
+      );
+    }
+    if (Platform.isIOS) {
+      await showCupertinoModalPopup<CupertinoDatePicker>(
+        context: context,
+        builder: (context) {
+          return Container(
+            height: 216,
+            padding: const EdgeInsets.only(top: 6.0),
+            margin: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            color: CupertinoColors.systemBackground.resolveFrom(context),
+            child: SafeArea(
+              top: false,
+              child: CupertinoDatePicker(
+                initialDateTime: DateTime.now(),
+                mode: CupertinoDatePickerMode.date,
+                use24hFormat: true,
+                onDateTimeChanged: (newDate) {
+                  selectDate = newDate;
+                },
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    return selectDate;
   }
 }
